6040e2dd72b8b9bad5a0751935644209fba7c535
zhuling <thezhuling@gmail.com>
2022-04-08 21:28:45 +0800

    添加hystrix-dashboard服务监控 Turbine聚合监控

--- hystrix-dashboard/src/main/java/com/zhuling/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/zhuling/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,10 +2,14 @@ package com.zhuling.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableHystrix
+@EnableTurbine
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {

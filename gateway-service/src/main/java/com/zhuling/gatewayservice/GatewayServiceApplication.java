ce15e3ed3060f0b73b1e0c24c31319d8218716ac
zhuling <thezhuling@gmail.com>
2022-04-10 14:28:40 +0800

    搭建SpringCloudGateway服务网关

---
+++ gateway-service/src/main/java/com/zhuling/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package com.zhuling.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("person_service",r-> r.path("/person/**").uri("lb://person-service"))
+                .build();
+    }
+}
